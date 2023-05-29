4a71f31e9e1b7a13fd0fda4e01501d7a3bd92be7
Walisson Martins <wm.silva@apoioprodesp.sp.gov.br>
2023-05-29 17:40:34 -0300

    feat: adicionando função de calculo e xml da interface do gasto viagem

--- app/src/main/java/com/walissonm/gastoviagem/MainActivity.kt
+++ app/src/main/java/com/walissonm/gastoviagem/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.walissonm.gastoviagem
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.walissonm.gastoviagem.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonCalculate.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        if (view?.id == R.id.button_calculate) {
+            calculate()
+        }
+    }
+
+    private fun isValid(): Boolean {
+        return (binding.editDistance.text.toString() != ""
+                && binding.editPrice.text.toString() != ""
+                && binding.editAutonomy.text.toString() != ""
+                && binding.editAutonomy.text.toString().toFloat() != 0f)
+    }
+
+    private fun calculate() {
+        if (isValid()) {
+            val distance = binding.editDistance.text.toString().toFloat()
+            val price = binding.editPrice.text.toString().toFloat()
+            val autonomy = binding.editAutonomy.text.toString().toFloat()
+
+            val totalValue = (distance * price) / autonomy
+            binding.textTotalValue.text = "R$ ${"%.2f".format(totalValue)}"
+        } else {
+            Toast.makeText(this, R.string.validation_fill_all_fields, Toast.LENGTH_LONG).show()
+        }
     }
 }
