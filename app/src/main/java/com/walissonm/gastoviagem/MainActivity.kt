1f79b8d4591d0c821d35a6c67d5fac15222fa045
Walisson Martins <wm.silva@apoioprodesp.sp.gov.br>
2023-05-31 16:25:51 -0300

    feat: constraintLayout e circle e constraint

--- app/src/main/java/com/walissonm/gastoviagem/MainActivity.kt
+++ app/src/main/java/com/walissonm/gastoviagem/MainActivity.kt
@@ -13,33 +13,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.buttonCalculate.setOnClickListener(this)
     }
 
-    override fun onClick(view: View?) {
-        if (view?.id == R.id.button_calculate) {
-            calculate()
-        }
-    }
-
-    private fun isValid(): Boolean {
-        return (binding.editDistance.text.toString() != ""
-                && binding.editPrice.text.toString() != ""
-                && binding.editAutonomy.text.toString() != ""
-                && binding.editAutonomy.text.toString().toFloat() != 0f)
-    }
-
-    private fun calculate() {
-        if (isValid()) {
-            val distance = binding.editDistance.text.toString().toFloat()
-            val price = binding.editPrice.text.toString().toFloat()
-            val autonomy = binding.editAutonomy.text.toString().toFloat()
-
-            val totalValue = (distance * price) / autonomy
-            binding.textTotalValue.text = "R$ ${"%.2f".format(totalValue)}"
-        } else {
-            Toast.makeText(this, R.string.validation_fill_all_fields, Toast.LENGTH_LONG).show()
-        }
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
     }
 }
